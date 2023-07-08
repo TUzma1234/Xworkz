4b44e0476cb8cbd7433be8e74c99936a9cab82a6
TUzma1234 <138605397+TUzma1234@users.noreply.github.com>
2023-07-08 02:01:51 -0700

    Add files via upload

---
+++ Test 2.java
@@ -0,0 +1,77 @@
+                   Amplitude Test
+Grandson
+Father in law
+Cousin
+Maternal uncle
+Nephew
+X/A*Y
+p
+Brother in law
+Brother
+T
+10kms
+East
+North-East
+West
+
+
+       java test
+	   
+	   
+	   
+	 1)  True
+	  2) 10,110
+	  3)false
+	 4) The main method is static because so that JVM will invoke instanting in the class.
+	 7)done
+	 8)this keyword is used to refer to the current object which the method is called.
+	   this() is the keyword to the constator from the same class.
+	 9)class Flower
+{
+	System.out.println("before swapping");
+	System.out.println("value of x is :"+x);
+	System.out.println("value of y is :"+y);
+		x=x+y;
+		y=x-y;
+		x=x-y;
+	
+	System.out.println("before swapping");
+	System.out.println("value of x is :"+=x);
+	System.out.println("value of y is :"+=y);
+	}
+	}
+10) 1) for(inti=0,i<now,i++)
+     {
+	 for(intj=0,j<now,j++)
+	 {
+	 for(intk=0,k<now,k++)
+	 }
+	   
+	   
+	   
+	2)  class Student
+	 {
+		public static void main(String [] args)
+		{
+		float a("the result"%","%",student result");
+		{
+		float b("the result a"%","&","%"student result);
+		{
+		float c("the result a"%","%","%",student result);
+		}
+	}
+	
+	
+	6) this RemoveDuplicateArry{
+		public static void main(String[] args)
+		{
+			if(n==0)(n==1);
+				return;
+			}
+				int[]temp=new int(0);
+				int j=0;
+			for int (i=0,i<0,i++);
+			{
+				
+			
+	
